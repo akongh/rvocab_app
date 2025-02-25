c32b17b3a5f182d609f145c79801cf5c95087f44
Andrei Korzhyts <andreikcontact@gmail.com>
2025-02-25 16:20:04 +0300

    refact

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,13 @@ use std::io::prelude::*;
 use std::process;
 use std::time::Instant;
 //todo: add tests
-//todo: comments for html making
 //todo: variable order for html making
 //todo: regex & z
 
 const RVOCAB_VERSION: &str = "0.1.0";
 
 const DEFAULT_INPUT_FILE_NAME: &str = "text.txt";
+const DEFAULT_OUTPUT_FILE_NAME: &str = "rvocab.html";
 
 const ERR_INPUT_NAME: &str = ">>>>>>>> No input file name given.\n";
 const ERR_OPEN: &str = ">>>>>>>> Can't open file!\n";
@@ -42,7 +42,7 @@ fn main() {
         input_file_name = input_args.nth(1).expect(ERR_INPUT_NAME);
     }
 
-    // Obtaining text from a text.txt.
+    // Obtaining text from a input file.
     let mut file = File::open(input_file_name).expect(ERR_OPEN);
     let mut raw_vocab = String::new();
     file.read_to_string(&mut raw_vocab).expect(ERR_READ);
@@ -131,24 +131,24 @@ fn main() {
 
     // MAIN LOGIC END
 
-    // Making data for a template.
+    // Making additional data for a template.
     let count = rvocab_vec_string.len();
     let title = "Total unique words: ".to_string() + &count.to_string();
     let date_time = Local::now().format("%Y-%m-%d %H:%M:%S").to_string();
     let rvocab = rvocab_vec_string.join("\n");
 
-    // Filling out the template.
-    let mut html_open = File::open("src/rvocab.html").expect(ERR_OPEN);
+    // Obtaining markup from a template.
+    let mut html_open = File::open("src/" + DEFAULT_OUTPUT_FILE_NAME).expect(ERR_OPEN);
     let mut html_template = String::new();
     html_open
         .read_to_string(&mut html_template)
         .expect(ERR_READ);
 
+    // Filling out the template.
     let r_version = Regex::new(r"\{\{version}}").unwrap();
     let r_date_time = Regex::new(r"\{\{date_time}}").unwrap();
     let r_title = Regex::new(r"\{\{title}}").unwrap();
     let r_rvocab = Regex::new(r"\{\{rvocab}}").unwrap();
-
     html_template = r_version
         .replace_all(&mut html_template, RVOCAB_VERSION)
         .to_string();
@@ -158,8 +158,8 @@ fn main() {
     html_template = r_title.replace_all(&mut html_template, title).to_string();
     html_template = r_rvocab.replace_all(&mut html_template, rvocab).to_string();
 
-    // Making ready-made HTML-markup and writing it to a rvocab.html.
-    let mut file = File::create("rvocab.html").expect(ERR_CREATE);
+    // Making ready-made HTML-markup and writing it to a output html.
+    let mut file = File::create(DEFAULT_OUTPUT_FILE_NAME).expect(ERR_CREATE);
     file.write_all(html_template.as_bytes()).expect(ERR_WRITE);
 
     println!("{MSG_WRITTEN}");
