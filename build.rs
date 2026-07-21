2e8e51475cbfd57af688b24652a99fbb71e13f76
Andrei Korzhyts <andreikcontact@gmail.com>
2026-07-21 14:34:36 +0300

    Repair build.rs.

--- build.rs
+++ build.rs
@@ -3,22 +3,23 @@ use std::process::Command;
 
 fn main() {
     if cfg!(target_os = "windows") {
-        // Путь к файлам
-        let rc_path = "src/resources/app.rc";
-        let res_path = "src/resources/resource.res";
+        let rc_path = Path::new("src/resources.rc");
+        let res_path = Path::new("src/resource.res");
 
-        // Компилируем app.rc в resource.res с помощью windres
+        if !rc_path.exists() {
+            panic!("File {} does not exist.", rc_path.display());
+        }
+
         let windres_status = Command::new("windres")
-            .args(&[rc_path, res_path])
+            .args(&[rc_path.to_str().unwrap(), res_path.to_str().unwrap()])
             .status()
             .expect("Failed to run windres. Make sure it is installed and in PATH.");
 
         if !windres_status.success() {
-            panic!("windres failed to compile the .rc file");
+            panic!("windres failed to compile the .rc file.");
         }
 
-        // Передаём resource.res линковщику
-        println!("cargo:rustc-link-arg={}", res_path);
-        println!("cargo:rerun-if-changed={}", rc_path);
+        println!("cargo:rustc-link-arg={}", res_path.display());
+        println!("cargo:rerun-if-changed={}", rc_path.display());
     }
 }
