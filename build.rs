f245a02a10e02c08a5026ae5aaeefe6776fa90ce
Andrei Korzhyts <andreikcontact@gmail.com>
2026-07-21 14:28:29 +0300

    Improving build.rs/

--- build.rs
+++ build.rs
@@ -1,6 +1,24 @@
+use std::path::Path;
+use std::process::Command;
+
 fn main() {
     if cfg!(target_os = "windows") {
-        println!("cargo:rustc-link-arg=src/resource.res");
-        println!("cargo:rerun-if-changed=src/resource.res");
+        // Путь к файлам
+        let rc_path = "src/resources/app.rc";
+        let res_path = "src/resources/resource.res";
+
+        // Компилируем app.rc в resource.res с помощью windres
+        let windres_status = Command::new("windres")
+            .args(&[rc_path, res_path])
+            .status()
+            .expect("Failed to run windres. Make sure it is installed and in PATH.");
+
+        if !windres_status.success() {
+            panic!("windres failed to compile the .rc file");
+        }
+
+        // Передаём resource.res линковщику
+        println!("cargo:rustc-link-arg={}", res_path);
+        println!("cargo:rerun-if-changed={}", rc_path);
     }
 }
