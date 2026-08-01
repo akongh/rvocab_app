93472e92525e37cdadd3bf22e09305235d7ef88f
Andrei K. <andreikcontact@gmail.com>
2026-08-01 13:08:29 +0300

    Improved build.rs & cleanall.rs.

--- build.rs
+++ build.rs
@@ -1,25 +1,30 @@
+use std::env;
 use std::path::Path;
 use std::process::Command;
 
 fn main() {
-    if cfg!(target_os = "windows") {
-        let rc_path = Path::new("src/resource.rc");
-        let res_path = Path::new("src/resource.res");
+    let bin_name = env::var("CARGO_BIN_NAME").unwrap_or_default();
 
-        if !rc_path.exists() {
-            panic!("File {} does not exist.", rc_path.display());
+    if bin_name == "" {
+        if cfg!(target_os = "windows") {
+            let rc_path = Path::new("src/resource.rc");
+            let res_path = Path::new("src/resource.res");
+
+            if !rc_path.exists() {
+                panic!("File {} does not exist.", rc_path.display());
+            }
+
+            let windres_status = Command::new("windres")
+                .args(&[rc_path.to_str().unwrap(), res_path.to_str().unwrap()])
+                .status()
+                .expect("Failed to run windres. Make sure it is installed and in PATH.");
+
+            if !windres_status.success() {
+                panic!("windres failed to compile the resource.rc file.");
+            }
+
+            println!("cargo:rustc-link-arg={}", res_path.display());
+            println!("cargo:rerun-if-changed={}", rc_path.display());
         }
-
-        let windres_status = Command::new("windres")
-            .args(&[rc_path.to_str().unwrap(), res_path.to_str().unwrap()])
-            .status()
-            .expect("Failed to run windres. Make sure it is installed and in PATH.");
-
-        if !windres_status.success() {
-            panic!("windres failed to compile the .rc file.");
-        }
-
-        println!("cargo:rustc-link-arg={}", res_path.display());
-        println!("cargo:rerun-if-changed={}", rc_path.display());
     }
 }
--- cleanall.rs
+++ cleanall.rs
@@ -2,14 +2,14 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    let paths_to_remove = vec![
-        "src/resource.res",
-        "rvocab.html",
-    ];
+    let paths_to_remove = vec!["src/resource.res", "rvocab.html"];
 
     for path in paths_to_remove {
+        // println!("{}", path);
         if Path::new(path).exists() {
-            fs::remove_file(path).expect("Failed to remove file.");
+            fs::remove_file(path).expect(&format!("> Failed to remove file: {}.", path));
+        } else {
+            eprintln!("> File does not exist: {}.", path);
         }
     }
 }
